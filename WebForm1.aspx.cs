397b6d8415521e6abf168fd6e7bf710cdefd634e
Jorge A. Calderon <67562342+jfnxcode@users.noreply.github.com>
2024-06-17 07:54:55 -0400

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace E42A
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string nombre = Request.Params["nombre"].ToString();
+            string edad = Request.Params["apellido"].ToString();
+            Response.Write("<h3> Nombre:  " + nombre + "</h3>");
+            Response.Write("<p> Edad:   " + edad + "</p> ");
+        }
+    }
+}
